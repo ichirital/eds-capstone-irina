68051b1b598b7423a0e7c18023517f5762999a00
Irina Chirita <chirita@adobe.com>
2025-02-21 11:14:54 +0000

    Linting issues

--- blocks/social-links/social-links.js
+++ blocks/social-links/social-links.js
@@ -1,21 +1,14 @@
 export default async function decorate(block) {
   const links = {};
-
-  console.log("Test")
-
+  
   // Extract links from the block's content.  Assumes structure:
   // <div><a href="facebook-link">Facebook</a></div>
   // <div><a href="twitter-link">Twitter</a></div>
   // <div><a href="instagram-link">Instagram</a></div>
 
-  console.log(block.innerHTML)
-
   block.querySelectorAll('div a').forEach(a => {
     const platform = a.textContent.toLowerCase(); // Get platform name
     const href = a.href;
-
-    console.log(platform + " : " + href)
-
     if (platform && href) {
       links[platform] = href;
     }
@@ -26,7 +19,7 @@ export default async function decorate(block) {
     return;
   }
 
-  let socialLinksHTML = '<div class="social-links-container">'; // 
+  let socialLinksHTML = '<div class="social-links-container">'; 
 
   for (const platform in links) {
     const href = links[platform];
@@ -36,7 +29,7 @@ export default async function decorate(block) {
       <a href="${href}" target="_blank" rel="noopener noreferrer" class="social-link">
         <span class="social-icon ${iconClass}"><img data-icon-name="search" src="/icons/${platform}.svg" alt="" loading="lazy"></span>
       </a>
-    `; //  <span class="social-icon ${iconClass}">${platform}</span>
+    `;
   }
 
   socialLinksHTML += '</div>';
@@ -44,7 +37,3 @@ export default async function decorate(block) {
   console.log(socialLinksHTML);
   block.innerHTML = socialLinksHTML;
 }
-
-
-
-// /blocks/social-links/social-links.css
